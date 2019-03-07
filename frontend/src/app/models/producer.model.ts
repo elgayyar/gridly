a951722cbee4fa2015564c77b1622425d1e8a181
ahart45 <ahart45@uwo.ca>
2019-03-07 17:26:14 -0500

    Created register ui

---
+++ frontend/src/app/models/producer.model.ts
@@ -0,0 +1,14 @@
+export class ProducerModel {
+    fName: String;
+    lName: String;
+    email: String;
+    homePhoneNo: number;
+    mobilePhoneNo: number;
+    streetNo: number;
+    streetName: String;
+    city: String;
+    postalCode: String;
+    province: String;
+    country: String;
+    producerStatus: boolean;
+}
