bb6225b437349c7719a6d47efcf372c2d7e0da2c
Nicholas Vukelic <nvukelic@uwo.com>
2019-03-12 02:14:13 -0400

    pulling consumers and sorting

--- frontend/src/app/services/trade.service.ts
+++ frontend/src/app/services/trade.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+import { environment } from '../../environments/environment'
+import { forkJoin, Observable,Subject,of, interval } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TradeService {
 
-  constructor() { }
+  authDomain = environment.authAPI;
+  ledgerDomain = environment.ledgerAPI;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  //GET all consumers
+  getAllConsumers() {
+    return this.http.get<JSON>(this.ledgerDomain + '/gridly.consumer.Consumer');
+  }
+
 }
