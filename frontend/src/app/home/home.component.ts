e405f6b12d3935a4a3f2b1187f5b0f65659a41a9
ahart45 <ahart45@uwo.ca>
2019-03-13 22:54:58 -0400

    Started on dashboard

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -11,7 +11,8 @@ import { Router } from "@angular/router";
 })
 export class HomeComponent implements OnInit {
   userProfile;
-
+  isConsumer;
+  
   constructor(private authService: AuthService,
     private router: Router,
     private authGuard: AuthGuard){ }
@@ -20,8 +21,13 @@ export class HomeComponent implements OnInit {
     console.log("Token", localStorage.getItem("token"));
     this.userProfile = JSON.parse(localStorage.getItem("activeProfile"));
     console.log(this.userProfile);
+    //Check is the user is a producer or consumer
+    if(this.userProfile.accountStatus == "CONSUMER") {
+      this.isConsumer = true;
+    } else {
+      this.isConsumer = false;
+    }
+    console.log("Account status", this.isConsumer);
   }
-
-
 
 }
---
+++ frontend/src/app/production-modal/production-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-production-modal',
+  templateUrl: './production-modal.component.html',
+  styleUrls: ['./production-modal.component.css']
+})
+export class ProductionModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
